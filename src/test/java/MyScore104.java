995bea556e5afdd9bf26dc1c29bdf87ca52efdf6
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-15 13:16:58 +0100

    A feladat és tesztek

---
+++ src/test/java/MyScore104.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public class MyScore104 {
+
+   @Test
+   public void test() {
+      String newLine="";
+      if (System.getProperty("os.name").startsWith("Windows")) {
+         newLine="\r\n";
+      } else {
+         newLine="\n";
+      }
+
+      InputStream stdin = System.in;
+
+      int score=104;
+
+      String input=String.valueOf(score)+newLine;
+      System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      PrintStream ps = new PrintStream(byteArrayOutputStream);
+      PrintStream stdout = System.out;
+      System.setOut(ps);
+
+      MyScore.main(new String[0]);
+
+      System.setIn(stdin);
+      System.setOut(stdout);
+
+      String expected="A kulonbseg:-4"+newLine;
+      String actual=byteArrayOutputStream.toString();
+
+      System.out.println("Elvart:"+newLine+expected);
+      System.out.println("Aktualis:"+newLine+actual);
+
+      boolean found=actual.contains(expected);
+      Assertions.assertTrue(found,"Nem jol hatarozta meg a kulonbseget!");
+
+      expected="Az elteres:4"+newLine;
+
+      System.out.println("Elvart:"+newLine+expected);
+      System.out.println("Aktualis:"+newLine+actual);
+
+      found=actual.contains(expected);
+      Assertions.assertTrue(found,"Nem jol hatarozta meg az elterest.");
+
+   }
+}
---
+++ src/test/java/TestInputText.java
@@ -0,0 +1,50 @@
+   import org.junit.jupiter.api.Assertions;
+   import org.junit.jupiter.api.Test;
+
+   import java.io.ByteArrayInputStream;
+   import java.io.ByteArrayOutputStream;
+   import java.io.InputStream;
+   import java.io.PrintStream;
+
+   public class TestInputText {
+
+      @Test
+      public void testInput() {
+         
+         String newLine="";
+         if (System.getProperty("os.name").startsWith("Windows")) {
+            newLine="\r\n";
+         } else {
+            newLine="\n";
+         }
+
+         InputStream stdin = System.in;
+
+         int score=120;
+
+         String input=String.valueOf(score)+newLine;
+         System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+         PrintStream ps = new PrintStream(byteArrayOutputStream);
+         PrintStream stdout = System.out;
+         System.setOut(ps);
+
+         MyScore.main(new String[0]);
+
+         System.setIn(stdin);
+         System.setOut(stdout);
+
+
+         String outStream="On mennyit dobott:";
+         String expected=outStream;
+         String actual=byteArrayOutputStream.toString();
+
+         System.out.println("Elvart:"+newLine+expected);
+         System.out.println("Aktualis:"+newLine+actual);
+
+         boolean found=actual.contains(expected);
+
+         Assertions.assertTrue(found,"Nem a megfelelo szoveggel kerte be a feladatot!");
+      }
+   }
